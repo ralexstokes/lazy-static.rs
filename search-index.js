62f10055fb4bd594e0b75ac71022ee9e26116504
Travis CI User <travis@example.org>
2018-12-06 02:45:58 +0000

    Documentation for rust-lang-nursery/lazy-static.rs@b4b2b16

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var N = null;var searchIndex = {};
+searchIndex["lazy_static"]={"doc":"A macro for declaring lazily evaluated statics.","items":[[5,"initialize","lazy_static","Takes a shared reference to a lazy static and initializes it if it has not been already.",N,[[["t"]]]],[8,"LazyStatic","","Support trait for enabling a few common operation on lazy static values.",N,N],[14,"lazy_static","","",N,N]],"paths":[]};
+initSearch(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,2 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['lazy_static'] = {"name":"","dirs":[],"files":["core_lazy.rs","lib.rs"]};
